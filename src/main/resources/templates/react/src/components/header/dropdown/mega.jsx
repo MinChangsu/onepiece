ff9b68ffb813813ae584d2ca5ff42910f0127817
민경서 <mgs1527@icloud.com>
2024-09-26 02:22:44 +0900

    링크 수정

--- src/main/resources/templates/react/src/components/header/dropdown/mega.jsx
+++ src/main/resources/templates/react/src/components/header/dropdown/mega.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {LinkProps} from "react-router-dom";
+import {LinkProps, NavLink} from "react-router-dom";
 import Menu from "../../../config/app-menu";
 
 function DropdownMegaMenu(){
@@ -9,9 +9,9 @@ function DropdownMegaMenu(){
 				{Menu.map((item, index) => (
 					<div className="navBox d-flex align-items-center" style={{height:'100%'}}>
 						<div key={index} className="navbar-item align-items-center justify-content-center" >
-							<a href={item.path} className="navbar-link d-flex align-items-center">
+							<NavLink to={item.path} className="navbar-link d-flex align-items-center">
 								<span style={{fontSize:'1.1rem',fontWeight:'bold'}}>{item.title}</span>
-							</a>
+							</NavLink>
 						</div>
 					</div>
 				))}
